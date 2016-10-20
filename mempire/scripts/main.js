0c688766e1d2fe0e112db51bea106533eddb057b
Heriberto Sosa <sosaheriberto2001@gmail.com>
2016-10-20 17:12:09 -0400

    Primer commit
    Primer commit

---
+++ mempire/scripts/main.js
@@ -0,0 +1,10 @@
+console.log('L.A.W');
+
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find('.more-less')
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
